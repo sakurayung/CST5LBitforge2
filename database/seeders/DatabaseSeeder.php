f157c04e5c04509cd1205da56e8b1c97d88c4750
sakurayung <jorspacturan13@gmail.com>
2025-05-23 15:04:49 +0800

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(FixMigrationsSeeder::class);
-        $this->call(CreateMissingTablesSeeder::class);
-
         // User::factory(10)->create();
 
         User::create([
