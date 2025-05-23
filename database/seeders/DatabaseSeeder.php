04558bedd19171d6cc05e1220d477517664f3640
sakurayung <jorspacturan13@gmail.com>
2025-05-23 13:30:28 +0800

    added fix migrations seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(FixMigrationsSeeder::class);
+
         // User::factory(10)->create();
 
         User::create([
